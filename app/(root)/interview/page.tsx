58fa3d1df79ee64b12f1203b03ee4aa803e9898d
sharma140345-sudo <sharma140345@gmail.com>
2026-03-17 01:06:24 +0530

    solved git issue

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -4,17 +4,12 @@ import { getCurrentUser } from '@/lib/actions/auth.action'
 
 const Page = async() => {
   const user = await getCurrentUser();
-  return (
-    <div>
-<<<<<<< HEAD
-      <h3>Interview Generation</h3>
-      <Agent userName={user?.name ?? ''} userId={user?.id ?? ''} type="generate" />
-=======
-        <h3>Interview Generation</h3>
-        <Agent userName={user?.name ?? ''} userId={user?.id} type="generate"/>
->>>>>>> 002d4d513950b6d4af250870cdd03285ac2df113
-    </div>
-  )
+return (
+  <div>
+    <h3>Interview Generation</h3>
+    <Agent userName={user?.name ?? ''} userId={user?.id ?? ''} type="generate" />
+  </div>
+)
 }
 
 export default Page
