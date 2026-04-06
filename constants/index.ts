204af0e6af3a2053ff5fa67e1702219cc40f1b34
sharma140345-sudo <sharma140345@gmail.com>
2026-04-06 08:34:20 +0530

    feedbaack schema

--- constants/index.ts
+++ constants/index.ts
@@ -155,37 +155,29 @@ export const mappings = {
 //   },
 // };
 
+const categoryNameEnum = z.enum([
+  "Communication Skills",
+  "Technical Knowledge",
+  "Problem Solving",
+  "Cultural Fit",
+  "Confidence and Clarity",
+]);
+
 export const feedbackSchema = z.object({
-  totalScore: z.number(),
-  categoryScores: z.tuple([
-    z.object({
-      name: z.literal("Communication Skills"),
-      score: z.number(),
-      comment: z.string(),
-    }),
-    z.object({
-      name: z.literal("Technical Knowledge"),
-      score: z.number(),
-      comment: z.string(),
-    }),
-    z.object({
-      name: z.literal("Problem Solving"),
-      score: z.number(),
-      comment: z.string(),
-    }),
-    z.object({
-      name: z.literal("Cultural Fit"),
-      score: z.number(),
-      comment: z.string(),
-    }),
-    z.object({
-      name: z.literal("Confidence and Clarity"),
-      score: z.number(),
-      comment: z.string(),
-    }),
-  ]),
-  strengths: z.array(z.string()),
-  areasForImprovement: z.array(z.string()),
+  totalScore: z.number().min(0).max(100),
+
+  categoryScores: z
+    .array(
+      z.object({
+        name: categoryNameEnum,
+        score: z.number().min(0).max(100),
+        comment: z.string(),
+      })
+    )
+    .length(5),
+
+  strengths: z.array(z.string()).min(1),
+  areasForImprovement: z.array(z.string()).min(1),
   finalAssessment: z.string(),
 });
 
